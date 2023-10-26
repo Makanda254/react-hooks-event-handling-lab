0809ca81d40ec8f5ac50a19adce4c48da2e2edaa
Victor <victormakanda254@gmail.com>
2023-10-26 21:32:52 +0300

    First Commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    function handleFocus(){
+       console.log('Good!');
+    }
+
+    function handleBlur(){
+       console.log('Hey! Eyes on me!')
+    }
+  return (
+    <div>
+      <button onFocus={(e)=> handleFocus(e)} onBlur={(e)=> handleBlur(e)}>Eyes on me</button>
+    </div>
+  )
+}
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,26 @@
 // Code Keypad Component Here
+import React, {useState} from "react";
 
 function Keypad (){
+    const[password, setPassword]= useState();
+  
+    
+    function handleChange(){
+
+        console.log('Entering password...')
+        
+    }
     return (
-        <div></div>
+        
+        <div>
+            <form>
+            <input type="password" onChange={(e)=> handleChange(()=>{
+                e.preventDefault() 
+                setPassword(e.target.value)
+                
+            }) } value={password}/>
+            </form>
+        </div>
     )
 }
 
